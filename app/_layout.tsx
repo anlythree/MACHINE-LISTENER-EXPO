27fb5497f9818f545f7631a31aa36b90d9c55eff
anlythree <wli@weds.com.cn>
2025-05-27 22:10:56 +0800

    code sync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,36 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import JPush from "jpush-react-native";
+import {useEffect} from "react";
 
 export default function RootLayout() {
+
+    useEffect(()=>{
+        console.log("进入配置页面");
+        JPush.init({
+            appKey: "5bf209ad3ea54ff8c4b0539e",
+            production: true,
+            channel: "default",
+        });
+
+        JPush.getRegistrationID((id) => {
+            console.log("RegistrationID: ", id);
+        });
+
+        const onConnect = (res) => {
+            console.log("监听回调", res);
+        };
+
+        JPush.addConnectEventListener(onConnect);
+
+        console.log("进入配置页面2");
+
+        return () => {
+            JPush.removeListener(onConnect);
+        };
+    },[])
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
